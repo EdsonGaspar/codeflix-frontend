4449a13fcab854bc4ae9cb3cdbd6aebe1433f84f
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2025-07-31 14:20:53 +0100

    chero: Add configuration ULTRACITE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Code Flix</h1>
+    <main className="flex flex-col gap-5 border border-black bg-accent px-1 font-semibold text-2xl text-neutral-50">
+      <h1>CodeFlia</h1>
     </main>
   );
 }
