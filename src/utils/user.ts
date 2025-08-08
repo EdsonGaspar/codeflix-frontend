fa12730eb8b011a6df3208e16ef859f13b5f5872
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2025-08-08 19:02:52 +0100

    feat: Consume file from backend

---
+++ src/utils/user.ts
@@ -0,0 +1,29 @@
+export type User = {
+  id: string;
+  name: string;
+  userName: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  age: string;
+  company: Company;
+};
+
+export type Address = {
+  city: string;
+  street: string;
+  suite: string;
+  zipcode: string;
+  geo: Geo;
+};
+
+export type Company = {
+  name: string;
+  catchPhrase: string;
+};
+
+export type Geo={
+  lat: string,
+  lng: string,
+}
