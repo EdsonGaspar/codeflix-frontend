1894b7bf494f918f20d718094211c7685fc765c8
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2025-08-09 09:59:14 +0100

    feat: Implemented page not-found

--- src/app/dashboard/user/[slug]/not-found.tsx
+++ src/app/dashboard/user/[slug]/not-found.tsx
@@ -3,7 +3,7 @@ import { H1 } from "@/components/h1";
 export default function NotFound() {
   return (
     <div className="flex items-center justify-center">
-      <H1>Pagina não encontrada</H1>
+      <H1>Usaurio não encontrado </H1>
     </div>
   );
 }
