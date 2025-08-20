c9a5b32ef5a472ad680120cd4de94c49ddfeeae5
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2025-08-20 19:47:13 +0100

    feat: I don't know how i can resolve this problem...

--- src/app/(auth)/register/form-register/form-register.tsx
+++ src/app/(auth)/register/form-register/form-register.tsx
@@ -1,9 +1,42 @@
 "use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import FormAuth from "../../_form-auth/form-auth";
 
+type ServerErrors = {
+  message: string;
+};
+
 export default function FormRegister() {
-  const handleSubmit = (evt: React.FormEvent) => {
+  const router = useRouter();
+  const [, setErrors] = useState<string[]>([]);
+
+  const handleSubmit = async (evt: React.FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
+
+    const formDataRegister = new FormData(evt.currentTarget);
+    const name = formDataRegister.get("name");
+    const email = formDataRegister.get("email");
+    const password = formDataRegister.get("password");
+    const confirm_password = formDataRegister.get("confirm_password");
+
+    try {
+      const response = await fetch("../register/api", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password, confirm_password, name }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+        return;
+      }
+
+      const playload: ServerErrors[] = await response.json();
+      setErrors(playload.map((erro) => erro.message));
+    } catch (err) {
+      setErrors([`Erro insesperado no servidor, ${err}`]);
+    }
   };
   return <FormAuth onSubmit={handleSubmit} typeForm="register" />;
 }
